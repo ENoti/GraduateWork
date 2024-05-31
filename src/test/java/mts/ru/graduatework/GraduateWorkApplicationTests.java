10ef87a2cb822d17a5f9717685e78b314b638d81
ENoti <dazhuk2016@yandex.ru>
2024-05-31 14:41:29 +0300

    Initial commit

---
+++ src/test/java/mts/ru/graduatework/GraduateWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package mts.ru.graduatework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
